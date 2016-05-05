93a5574a66d6b3c95f81941bb7971fdf523cf9c1
Keks1k <a.kuplyaev@gmail.com>
2016-05-06 01:10:57 +0300

    readme

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # University
 Сам сайт http://k93078qv.bget.ru/
 
- user:136895
- password:12345
+ user:136895;
+ password:12345;
+
+ admin:0;
+ password:admin;
--- admin.php
+++ admin.php
@@ -7,12 +7,33 @@ else{
     require_once  'layouts/header.php';
 }
 ?>
-    <div class="container maps">
+    <div class="container-fluid maps">
+        <div class="row">
+            <div class="col-sm-3 col-md-2 sidebar">
+                <ul class="nav nav-sidebar">
+                    <li ><a href="#" onclick="$.post('', function(data)">Просмотр студентов</a></li>
+                    <li><a href="#">Reports</a></li>
+                    <li><a href="#">Analytics</a></li>
+                    <li><a href="#">Export</a></li>
+                </ul>
+                <ul class="nav nav-sidebar">
+                    <li><a href="">Nav item</a></li>
+                    <li><a href="">Nav item again</a></li>
+                    <li><a href="">One more nav</a></li>
+                    <li><a href="">Another nav item</a></li>
+                    <li><a href="">More navigation</a></li>
+                </ul>
+                <ul class="nav nav-sidebar">
+                    <li><a href="">Nav item again</a></li>
+                    <li><a href="">One more nav</a></li>
+                    <li><a href="">Another nav item</a></li>
+                </ul>
+            </div>
+        </div>
         <div id="admin">
             <form action="exit.php" method="post">
-             <h1>Вы зашли как: <?echo $_SESSION['fio'];?></h1>
                 <button type="submit" class="btn btn-primary">
-                        ВЫЙТИ
+                    ВЫЙТИ
                 </button>
             </form>
         </div>
